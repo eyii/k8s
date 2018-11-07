d50ca20ddb46720af0084eb7c5ee31223da3bc78
mudocker <mudocker@gmail.com>
2018-11-07 18:23:44 +0800

    1541580465

--- install/cluster/etcd.sh
+++ install/cluster/etcd.sh
@@ -17,9 +17,8 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
 
 ETCD_VERSION=v3.2.4
-echo "etcd version is $ETCD_VERSION"
+
 ETCD_FILE=etcd-$ETCD_VERSION-linux-amd64
-echo "etcd zip file is $ETCD_FILE"
 etd_file_url=https://github.com/coreos/etcd/releases/download/$ETCD_VERSION/$ETCD_FILE.tar.gz
 echo $etd_file_url
 if [ ! -f "./$ETCD_FILE.tar.gz" ]; then
@@ -42,7 +41,7 @@ cp $ETCD_FILE/etcdctl $ETCD_BIN_DIR
 rm -rf $ETCD_FILE
 
 sed -i 's/$PATH:/$PATH:\/opt\/kubernetes\/bin:/g' ~/.bash_profile
-#source ~/.bash_profile
+source ~/.bash_profile
 exec bash --login
 
 ETCD_DATA_DIR=/var/lib/etcd
@@ -53,66 +52,39 @@ ETCD_NAME=${1:-"default"}
 ETCD_LISTEN_IP=${2:-"0.0.0.0"}
 ETCD_INITIAL_CLUSTER=${3:-}
 
-echo 'Create /opt/kubernetes/cfg/etcd.conf ...'
+
 cat <<EOF >/opt/kubernetes/cfg/etcd.conf
 # [member]
 ETCD_NAME="${ETCD_NAME}"
 ETCD_DATA_DIR="${ETCD_DATA_DIR}/default.etcd"
-#ETCD_SNAPSHOT_COUNTER="10000"
-#ETCD_HEARTBEAT_INTERVAL="100"
-#ETCD_ELECTION_TIMEOUT="1000"
 ETCD_LISTEN_PEER_URLS="http://0.0.0.0:2380"
 ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379"
-#ETCD_MAX_SNAPSHOTS="5"
-#ETCD_MAX_WALS="5"
-#ETCD_CORS=""
-#
 #[cluster]
 ETCD_INITIAL_ADVERTISE_PEER_URLS="http://${ETCD_LISTEN_IP}:2380"
-# if you use different ETCD_NAME (e.g. test),
-# set ETCD_INITIAL_CLUSTER value for this name, i.e. "test=http://..."
 ETCD_INITIAL_CLUSTER="${ETCD_INITIAL_CLUSTER}"
 ETCD_INITIAL_CLUSTER_STATE="new"
 ETCD_INITIAL_CLUSTER_TOKEN="k8s-etcd-cluster"
 ETCD_ADVERTISE_CLIENT_URLS="http://${ETCD_LISTEN_IP}:2379"
-#ETCD_DISCOVERY=""
-#ETCD_DISCOVERY_SRV=""
-#ETCD_DISCOVERY_FALLBACK="proxy"
-#ETCD_DISCOVERY_PROXY=""
-#
 #[proxy]
-#ETCD_PROXY="off"
-#
 #[security]
-#ETCD_CA_FILE=""
-#ETCD_CERT_FILE=""
-#ETCD_KEY_FILE=""
-#ETCD_PEER_CA_FILE=""
-#ETCD_PEER_CERT_FILE=""
-#ETCD_PEER_KEY_FILE=""
 EOF
 
-echo 'Create /usr/lib/systemd/system/etcd.service ...'
-cat <<EOF >//usr/lib/systemd/system/etcd.service
+
+cat <<EOF >/usr/lib/systemd/system/etcd.service
 [Unit]
 Description=Etcd Server
 After=network.target
-
 [Service]
 Type=simple
 WorkingDirectory=${ETCD_DATA_DIR}
 EnvironmentFile=-/opt/kubernetes/cfg/etcd.conf
-# set GOMAXPROCS to number of processors
 ExecStart=/bin/bash -c "GOMAXPROCS=\$(nproc) /opt/kubernetes/bin/etcd"
 Type=notify
-
 [Install]
 WantedBy=multi-user.target
 EOF
-
 
 systemctl daemon-reload
 systemctl enable etcd
 systemctl restart etcd
 
-echo 'The etcd service is started!'
